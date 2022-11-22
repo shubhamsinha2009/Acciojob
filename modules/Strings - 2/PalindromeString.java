09c8cb7670459fe794ed5dea73eb0e63223f5a17
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-11-22 22:58:02 +0530

    Create PalindromeString.java

---
+++ modules/Strings - 2/PalindromeString.java
@@ -0,0 +1,51 @@
+
+// Question Link --- https://course.acciojob.com/idle?question=ad163b7c-8fea-42d3-a8e9-39d4d6a06b18
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        s =  s.toLowerCase();
+
+        int low = 0 ;
+        int high = s.length()-1;
+        
+
+        while(low < high ){
+
+            if(!Character.isLetterOrDigit(s.charAt(high))){
+                high -- ;
+                continue;
+            }
+              if(!Character.isLetterOrDigit(s.charAt(low))){
+                low++ ;
+                continue;
+            }
+            // System.out.println(s.charAt(low));
+            // System.out.println(s.charAt(high));
+
+            if(s.charAt(low) != s.charAt(high) ){
+                        System.out.println("0");
+                break;
+ 
+            }
+            low++;
+            high--;
+
+          
+        }
+
+          if(low>=high){
+                System.out.println("1");
+            }
+        
+	}
+}
