2033a65fa1e5ad5fa6ac12635751cf749aa1e6b9
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-11-23 01:21:36 +0530

    Create SuperHero.java

---
+++ modules/Functions/SuperHero.java
@@ -0,0 +1,21 @@
+
+// Questions Link --- https://course.acciojob.com/idle?question=8b8f2d0a-0502-4946-aa71-7f94fc20e857
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        System.out.println(fun(s));
+	}
+
+    public static String fun( String s){
+        return  s + " is a Super Hero" ;
+    }
+}
