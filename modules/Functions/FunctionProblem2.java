dfd6e53e9749659354371d8f555685a9f6925f8a
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-11-23 01:17:39 +0530

    Create FunctionProblem2.java

---
+++ modules/Functions/FunctionProblem2.java
@@ -0,0 +1,22 @@
+
+// Questions Link -- https://course.acciojob.com/idle?question=327be672-fa3d-4fdb-91f5-ac74ff96bdca
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        fun(a,b);
+        
+	}
+    public static void fun(int a , int b ){
+        System.out.println(a+b);
+    }
+}
