1f15475441f931a43f34e078d6091eed4c41c8ac
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-11-23 01:23:12 +0530

    Create FunctionProblem1.java

---
+++ modules/Functions/FunctionProblem1.java
@@ -0,0 +1,18 @@
+
+// Questions Link -- https://course.acciojob.com/idle?question=a5f70c37-5c04-48aa-877f-3e8d60e6b6bd
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		fun();
+	}
+
+    public static void fun(){
+        System.out.print("I am another function");
+    }
+}
