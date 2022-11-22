a9c93cb81253ed44e5e07455d023d4bcc501c26c
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-11-23 01:24:02 +0530

    Create SwitchCaseProblem1.java

---
+++ modules/Functions/SwitchCaseProblem1.java
@@ -0,0 +1,24 @@
+
+// Question Link -- https://course.acciojob.com/idle?question=b6708167-2376-41b4-bddb-495e403fd426
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+
+                switch(n){
+                        case 55:
+                                System.out.println("Study is important");
+                                break;
+                        default:   
+                                System.out.println("No value");
+                        
+                }
+	}
+}
