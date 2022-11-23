bfca96fe1137b713972588303e9c193450b2b019
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-11-23 18:15:58 +0530

    Create AltMatrixSum.java

---
+++ modules/1D & 2D Arrays/AltMatrixSum.java
@@ -0,0 +1,36 @@
+
+// Question Link -- https://course.acciojob.com/idle?question=0ca3a15a-7003-4c33-850f-7e1e65798015
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int mat[][] = new int[n][n];
+    int blackSum =0;
+    int whiteSum = 0;
+    for(int i =0; i<n; i++){
+      for(int j =0; j<n; j++ ){
+        mat[i][j] = sc.nextInt();
+      }
+    }
+    
+    for(int i =0; i<n; i++){
+     for(int j =0; j<n; j++ ){
+     
+      if ((i+j)%2==0){
+        blackSum += mat[i][j];
+      }else{
+               whiteSum += mat[i][j] ;
+      }
+     }
+    }
+    System.out.println(blackSum);
+                 System.out.println(whiteSum);
+	}
+}
