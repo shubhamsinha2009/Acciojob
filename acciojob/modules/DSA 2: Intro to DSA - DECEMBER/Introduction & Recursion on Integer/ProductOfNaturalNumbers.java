85bbfed35599ed19a6fedb38d6ea6799a3aed420
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-03 10:05:54 +0530

    Create ProductOfNaturalNumbers.java

---
+++ acciojob/modules/DSA 2: Intro to DSA - DECEMBER/Introduction & Recursion on Integer/ProductOfNaturalNumbers.java
@@ -0,0 +1,25 @@
+
+// Question Link - https://course.acciojob.com/idle?question=346dcf47-f0b0-48b9-9e25-c0a004837b34
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+               long fact = fact(n);
+              
+
+                System.out.println(fact);
+	}
+
+        public static long fact(int n)
+	{
+			if(n ==1) return 1;
+	        return n*fact(n-1);
+	}
+}
