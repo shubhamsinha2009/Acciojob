8d4e80191d1f95e5badfd755e0c7fa00764d515c
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-03 13:43:10 +0530

    Update RecursiveFibonacci.java

--- acciojob/modules/DSA 2: Intro to DSA - DECEMBER/Introduction & Recursion on Integer/RecursiveFibonacci.java
+++ acciojob/modules/DSA 2: Intro to DSA - DECEMBER/Introduction & Recursion on Integer/RecursiveFibonacci.java
@@ -7,10 +7,7 @@ import static java.lang.Math.ceil;
 public class Main {
 	
 	static int fib(int n ){
-		
-		if(n==1) return 0;
-		if(n==2) return 1;
-
+		if(n<=2) return n-1;
 		return fib(n-1) + fib(n-2);
     }
 	
