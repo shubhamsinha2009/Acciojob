0d34b915cef01f9598e96f56da4a8157cec6e2db
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-03 14:02:20 +0530

    Create NthEvenFibonacciNumber.java

---
+++ acciojob/modules/DSA 2: Intro to DSA - DECEMBER/Introduction & Recursion on Integer/NthEvenFibonacciNumber.java
@@ -0,0 +1,22 @@
+
+// Question Link - https://course.acciojob.com/idle?question=b9ab1535-75ae-4b9f-b6ca-a1ae2429d630
+
+import java.util.*;
+import static java.lang.Math.ceil;
+
+public class Main {
+	static final int mod = 1000000007;
+	
+	static long NthEvenFibonacci(long n) {
+
+		if(n<=1) return 2*n;
+		return (4*NthEvenFibonacci(n-1) + NthEvenFibonacci(n-2))%mod;
+	
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(NthEvenFibonacci(n));
+    }
+}
