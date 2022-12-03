3c49219fbf97d6017ce3bc0581c1b24da3dcc184
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-03 09:10:49 +0530

    Create RecursiveMultiplication.java

---
+++ acciojob/modules/DSA 2: Intro to DSA - DECEMBER/Introduction & Recursion on Integer/RecursiveMultiplication.java
@@ -0,0 +1,47 @@
+
+// https://course.acciojob.com/idle?question=04fc9c68-b5bc-45fd-a894-2818207deae8
+
+import java.io.*;
+import java.util.*;
+
+
+class Solution{
+  public static void multiplyRecursively(int n, int m) {
+
+	  
+	  // Time Complexity - O(min(n,m))
+	  //	 Space Complexity - O(min(n,m)) 
+	  // recursive stack memeory
+	  
+	  if(n>m){
+		   System.out.println(add(n,m));
+	  }else{
+		   System.out.println(add(m,n));
+	  }
+	 
+
+  }
+
+	public static int add(int n, int m){
+
+		if(m==1) return n;
+		
+		return n + add(n,m-1);
+	}
+	
+
+
+
+	
+}
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int m = scn.nextInt();
+    Solution ob = new Solution();
+    ob.multiplyRecursively(n, m);
+  }
+}
