404625f02b63cc8585974ebea61087889d068929
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-13 14:02:39 +0530

    Update QuickSort1.java

--- acciojob/modules/DSA 2: Intro to DSA - DECEMBER/Merge And Quick Sort/QuickSort1.java
+++ acciojob/modules/DSA 2: Intro to DSA - DECEMBER/Merge And Quick Sort/QuickSort1.java
@@ -4,26 +4,17 @@
 import java.io.*;
 import java.util.*;
 
+import java.io.*;
+import java.util.*;
+
 class Accio {
        static int[] quickSort(int a[], int n)
         {
 		int[] ans = new int[n]; 
 			int k = 0, val = a[0]; 
-			for(int i = 0; i < n; i++) 
-				if(a[i] < val){ 
-					ans[k] = a[i]; 
-					k++; 
-				} 
-			for(int i = 0; i < n; i++) 
-				if(a[i] == val){ 
-					ans[k] = a[i];
-					k++; 
-				}
-			for(int i = 0; i < n; i++) 
-				if(a[i] > val){
-					ans[k] = a[i]; 
-					k++; 
-				} 
+			for(int i : a) if(i<val) ans[k++] = i;
+			for(int i : a) if(i==val) ans[k++] = i;
+			for(int i : a) if(i>val) ans[k++] = i;
 			return ans;
         }  
 }
