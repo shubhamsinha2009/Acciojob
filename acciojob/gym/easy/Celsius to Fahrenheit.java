b237c3c939c233460513354f8e99fb1e94dce532
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-15 15:31:10 +0530

    Create Celsius to Fahrenheit.java

---
+++ acciojob/gym/easy/Celsius to Fahrenheit.java
@@ -0,0 +1,15 @@
+
+// Question Link -- https://course.acciojob.com/idle?question=0093b914-77cf-41b4-b792-63cc8388aafb
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) {
+       Scanner scanner = new Scanner(System.in);
+       int fah = scanner.nextInt();
+      
+      System.out.println(fah*9/5 + 32);
+    }
+}
