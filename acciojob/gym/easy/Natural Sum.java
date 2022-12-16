05a1614d900da00b178035bddb554643c80dd4ae
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-16 14:04:55 +0530

    Create Natural Sum.java

---
+++ acciojob/gym/easy/Natural Sum.java
@@ -0,0 +1,20 @@
+
+// Question Link - https://course.acciojob.com/idle?question=065a216f-83d8-480c-97ed-311ea0665dff
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+                Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+                // System.out.println(n*(n+1)/2);
+                int sum = n*(n+1)/2 ;
+               
+                System.out.println(sum);
+	}
+}
