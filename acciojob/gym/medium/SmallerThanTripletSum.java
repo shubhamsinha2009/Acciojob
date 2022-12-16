0eb183c044b298bb4d597406ecefa6548ee3d614
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-16 20:58:23 +0530

    Update SmallerThanTripletSum.java

--- acciojob/gym/medium/SmallerThanTripletSum.java
+++ acciojob/gym/medium/SmallerThanTripletSum.java
@@ -19,7 +19,22 @@ public class Main {
 }
 class Solution {
     static int threeSumSmaller(int n, int[] a, int target) {
-      
+
+		// Brute Force Solution 
+		// Time Complexity -- O(n^3)
+
+		// return bruteForce(n,a,target);
+
+		// Optimised Solution 
+		// Time Complexity - O(n^2)
+
+		return optimisedSol(n,a,target);
+		
+		
+		
+    }
+
+	static int bruteForce(int n, int[] a, int target){
 		int ans =0;
 		for(int i =0;i<n-2;i++){
 			for(int j=i+1;j<n-1;j++){
@@ -37,6 +52,27 @@ class Solution {
 			}
 		}
 		return ans ;
-		
-    }
+	}
+
+	static int optimisedSol(int n, int[] a, int target){
+
+		int ans =0;
+		Arrays.sort(a);
+		for(int i =0 ; i<n-2;i++){
+			
+			int low = i+1;
+			int high = n-1;
+			
+			while(low<high){
+
+				if(a[i]+a[low]+a[high]>=target){
+					high--;
+				} else{
+					ans+= high-low;
+					low++;
+				}
+			}
+		}
+		return ans ;
+	}
 }
