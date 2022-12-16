b056ef86cd6332fa7a14105f00f21fd78226b971
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-16 21:22:52 +0530

    Update and rename SumOfNaturalNumbers.java to Sum of Natural Numbers.java

---
+++ acciojob/gym/easy/Sum of Natural Numbers.java
@@ -0,0 +1,19 @@
+
+// Question Link -- https://course.acciojob.com/idle?question=0aa5b6cb-03f2-42c1-9ec9-e2f7c8257da3
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner scanner = new Scanner(System.in);
+    
+    long n = scanner.nextLong();
+    long sum = (long) (n*(n+1))/2 ;
+    
+    System.out.print(sum);
+	}
+}
