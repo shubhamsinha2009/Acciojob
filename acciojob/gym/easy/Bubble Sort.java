b0dcbbf9b0c623a965b89110538a5d057fab2667
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2022-12-16 13:47:21 +0530

    Create Bubble Sort.java

---
+++ acciojob/gym/easy/Bubble Sort.java
@@ -0,0 +1,58 @@
+
+// Question Link - https://course.acciojob.com/idle?question=04c51a0a-2136-40da-8738-c391d6a9ef8f
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int arr[] = new int[N];
+
+        // Input of array
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // bubble sort in descending order
+        bubbleSort(N, arr);
+        // printing array elements
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+	private static void swap(int a , int b , int[] arr){
+					int temp = arr[a];
+					arr[a] = arr[b];
+					arr[b] = temp;
+	}
+
+    public static void bubbleSort(int N, int[] arr) {
+
+		
+		for(int i = 0 ; i<N-1 ; i++){
+
+			boolean swap = false;
+			for(int j = 0 ; j<N-1-i; j++ ){
+
+				int currentElementIndex = j;
+				int nextElementIndex = j+1;
+				
+				if( arr[currentElementIndex] < arr[nextElementIndex]){
+					swap(currentElementIndex,nextElementIndex,arr);
+					swap = true;
+				}
+				
+			}
+			if(!swap){
+				break;
+			}
+		}
+
+		
+    }
+}
