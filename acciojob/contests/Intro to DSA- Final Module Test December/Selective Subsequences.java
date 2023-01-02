83c7ee786f17e41962977f0c2cf161423e2b504b
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2023-01-02 15:37:27 +0530

    Update Selective Subsequences.java

--- acciojob/contests/Intro to DSA- Final Module Test December/Selective Subsequences.java
+++ acciojob/contests/Intro to DSA- Final Module Test December/Selective Subsequences.java
@@ -5,68 +5,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+  public static void solver(String str,int i,String sub,ArrayList<String> ans){
+		if(i==-1){
+			if((sub.length()%2==0||sub.length()%7==0)&&sub.length()!=0){
+				ans.add(sub);
+				return;
+			}
+		}
+		if(i<=-1) return;
+
+		solver(str,i-1,sub,ans);
+
+		solver(str,i-1,str.charAt(i)+sub,ans);
+
+		
+	}
     public static ArrayList<String> generateSubsequences(String str)
-    {  
-		ArrayList<String> subSequences = getSequence(str);
-
-		
-        for(int i =1 ; i<subSequences.size();i++){
-            String sub = subSequences.get(i);
-            if(sub.length()%2!=0 && sub.length()%7!=0){
-                subSequences.remove(i--);
-            }
-		
-        }
-		subSequences.remove(0);
-        
-        
-        return subSequences;
-    }
-
-
-	public static ArrayList<String> getSequence(String str)
-{
-
-	// If string is empty
-	if (str.length() == 0) {
-
-		// Return an empty arraylist
-		ArrayList<String> empty = new ArrayList<>();
-		empty.add("");
-		return empty;
-	}
-
-	// Take first character of str
-	char ch = str.charAt(0);
-
-	// Take sub-string starting from the
-	// second character
-	String subStr = str.substring(1);
-
-	// Recurvise call for all the sub-sequences
-	// starting from the second character
-	ArrayList<String> subSequences =
-						getSequence(subStr);
-
-	// Add first character from str in the beginning
-	// of every character from the sub-sequences
-	// and then store it into the resultant arraylist
-	ArrayList<String> res = new ArrayList<>();
-	for (String val : subSequences) {
-      // System.out.println(val);
-	        res.add(val);
-        
-		 String g = ch+val;
-		//  System.out.println(g);
-			 res.add(g);
-			
-	
-        
-	}
-
-	// Return the resultant arraylist
-	return res;
-}
+    {
+        // write your code here
+		ArrayList<String> ans=new ArrayList<>();
+		solver(str,str.length()-1,"",ans);
+		return ans;
+    }
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s;
