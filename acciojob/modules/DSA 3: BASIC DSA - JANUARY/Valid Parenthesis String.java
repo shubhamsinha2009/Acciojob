c1da75e0b94afae32c53986112f861620f3cb96b
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2023-01-04 19:48:34 +0530

    Create Valid Parenthesis String.java

---
+++ acciojob/modules/DSA 3: BASIC DSA - JANUARY/Valid Parenthesis String.java
@@ -0,0 +1,33 @@
+
+// Questions Link -- https://course.acciojob.com/idle?question=d77837bf-ee1b-44d5-9c46-9942f3756bd8
+
+import java.util.*;
+
+class Solution{
+    public static boolean checkValidString(int n, String s){
+         int lo = 0, hi = 0;
+       for (char c: s.toCharArray()) {
+           lo += c == '(' ? 1 : -1;
+           hi += c != ')' ? 1 : -1;
+           if (hi < 0) break;
+           lo = Math.max(lo, 0);
+       }
+       return lo == 0;//write code here
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        String s = sc.next();
+        Solution obj = new Solution();
+        boolean ans = obj.checkValidString(n,s);
+        if(ans==true){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
