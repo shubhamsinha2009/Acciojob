f09b4598a1861c6c63919bf740ade7066bbac98c
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2023-01-13 22:48:40 +0530

    Create Substring With K Unique characters.java

---
+++ modules/DSA 3_ BASIC DSA - JANUARY/Hashing 3/Substring With K Unique characters.java
@@ -0,0 +1,47 @@
+
+// Question Link -- https://course.acciojob.com/idle?question=10944e43-a4d3-4974-9ea9-02aa61d602ee
+
+import java.util.*;
+
+public class Main {
+
+    public static int longestkSubstr(int n, int k,String s){
+
+		HashMap<Character,Integer> map = new HashMap<>();
+
+		int max =-1;
+		for(int i =0,j=0 ; i<n;i++){
+			char ch = s.charAt(i);
+
+			map.put(ch,map.getOrDefault(ch,0)+1);
+
+				while(map.size()>k){
+						char chh = s.charAt(j);
+						int value = map.get(chh) - 1;
+					if(value==0){
+						map.remove(chh);
+					}else{
+						map.put(chh,value);
+					}
+
+					j++;
+				}
+
+				if(map.size() == k){
+					max = Math.max(max,i-j+1);
+				}
+
+		}
+
+		return max;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String s = sc.next();
+        int ans = longestkSubstr(n,k,s);
+        System.out.println(ans);
+    }
+}
