79c4907224f784c37eb04025b3bd34f784311b89
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2023-01-17 17:33:02 +0530

    Create LRU Cache.java

---
+++ modules/DSA 3_ BASIC DSA - JANUARY/Hashing 4/LRU Cache.java
@@ -0,0 +1,94 @@
+
+// Question Link -- https://course.acciojob.com/idle?question=6a8c74ff-60f3-49a6-a929-5e7335416f8e
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cap = sc.nextInt(), q = sc.nextInt();
+        String[] queries = new String[q];
+        for (int i = 0; i < q; i++) {
+            String s = sc.next();
+            if (s.equals("GET"))
+                s += " " + sc.nextInt();
+            else
+                s += " " + sc.nextInt() + " " + sc.nextInt();
+            queries[i] = s;
+        }
+        sc.close();
+        LRUCache lruCache = new LRUCache(cap);
+        for (String s : queries) {
+            String[] arr = s.split(" ");
+            if (arr.length == 3)
+                lruCache.set(Integer.parseInt(arr[1]), Integer.parseInt(arr[2]));
+            else
+                System.out.println(lruCache.get(Integer.parseInt(arr[1])));
+        }
+    }
+}
+
+class LRUCache {
+    
+	HashMap<Integer,Node> map ;
+    Node head , tail;
+    int capacity;
+
+	
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        head.next = tail;
+        tail.prev= head;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        } else {
+            return -1;
+        }
+    }
+
+    public void set(int key, int value) {
+       if (map.containsKey(key)) {
+                remove(map.get(key));
+            }
+            if (map.size() == capacity) {
+                remove(tail.prev);
+            }
+            insert(new Node(key, value));
+    }
+
+	 private void insert(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
+
+       private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    class Node{
+    int key;
+    int value;
+    Node next;
+    Node prev;
+
+    Node(int key,int value){
+        this.key = key;
+        this.value = value;
+    }
+	}
+
+}
