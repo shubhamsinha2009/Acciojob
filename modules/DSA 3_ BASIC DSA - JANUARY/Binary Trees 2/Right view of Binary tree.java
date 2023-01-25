99ecdcce2bccae189054b9ef529f2e34ad887278
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2023-01-25 13:46:42 +0530

    Create Right view of Binary tree.java

---
+++ modules/DSA 3_ BASIC DSA - JANUARY/Binary Trees 2/Right view of Binary tree.java
@@ -0,0 +1,102 @@
+
+// Question Link -- https://course.acciojob.com/idle?question=21d3de29-2828-4145-8aa5-c9d8ab42ee78
+
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class Main {
+    static Node buildTree(String str){
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if(i >= ip.length)
+                break;   
+            currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+			String s = br.readLine();
+			Node root = buildTree(s);
+			Solution tree = new Solution();
+			ArrayList<Integer> arr = tree.rightView(root);
+			for(int x : arr)
+			System.out.print(x +" ");
+			System.out.println();  
+		}
+	}
+}
+  
+
+
+class Solution{
+    ArrayList<Integer> rightView(Node root) {
+	   ArrayList<Integer> list = new ArrayList<>();
+        Queue<Node> queue = new ArrayDeque<>();
+
+       if(root!=null) queue.offer(root);
+        int level = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for(int i=0;i<size;i++){
+                Node node= queue.poll();
+                  if(node.left!=null)  queue.offer(node.left);
+                  if(node.right!=null)  queue.offer(node.right); 
+                 
+                 if(list.size() == level+1) list.set(level,node.data);
+                 else list.add(node.data);
+            }
+            level++;
+        }
+        
+        return list;
+    }
+}
+
