3cb77ce496b780b7eef631644f1412ed6b7ae778
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2023-01-25 15:23:50 +0530

    Create Lowest Common Ancestor.java

---
+++ modules/DSA 3_ BASIC DSA - JANUARY/Binary Trees 3/Lowest Common Ancestor.java
@@ -0,0 +1,82 @@
+
+// Question link -- https://course.acciojob.com/idle?question=978382d5-aa6d-4dee-8a1a-b9bda9cd2ddc
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+
+
+class Main {
+    static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if (!currVal.equals("-1")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if (i >= ip.length) break;
+            currVal = ip[i];
+            if (!currVal.equals("-1")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Node root = buildTree(s);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        Solution g = new Solution();
+        Node ans = g.findLCA(root,x,y);
+        System.out.println(ans.data);
+    }
+}
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+    public static Node findLCA(Node root,int p,int q) {
+        if(root==null|| root.data==p || root.data==q) return root;
+
+           Node left = findLCA(root.left,p,q);
+           Node right = findLCA(root.right,p,q);
+
+            if(left==null){
+                return right;
+            }else if(right==null){
+                return left;
+            }else{
+                return root;
+            }
+        
+    }
+}
