01233de1e250de62a94ffd5e04dabac3a8abe8e3
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2023-02-07 12:18:45 +0530

    Create Insert a node in a BST.java

---
+++ modules/DSA 4: INTERMEDIATE DSA - FEBRUARY/BST 1/Insert a node in a BST.java
@@ -0,0 +1,80 @@
+
+// Question link -- https://course.acciojob.com/idle?question=e552caac-75e2-4f1e-8aee-a49ae6c08134
+
+
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int val;
+    Node left, right;
+    Node(int val){
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+class BST{
+    Node root = null;
+    BST(){
+        
+    }
+    BST(Node root){
+        root=root;
+    }
+    Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.val == val)
+            return root;
+        if(val < root.val){
+            root.left = insert(root.left, val);
+        }else{
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    void print(Node root){
+        System.out.print(root.val + " ");
+        if(root.left != null){
+            print(root.left);
+        }
+        if(root.right != null){
+            print(root.right);
+        }
+    }
+}
+
+class Solution{
+    public Node insertNode(Node root, int val){
+			if(root==null) return new Node(val);
+
+		 if(root.val == val)
+            return root;
+		
+			if(root.val > val){
+				root.left = insertNode(root.left,val);
+			}else{
+				root.right = insertNode(root.right,val);
+			}
+
+		return root;
+			
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt(), k = input.nextInt();
+        BST t = new BST();
+        for(int i = 0; i < n; i++){
+            t.root = t.insert(t.root, input.nextInt());
+        }
+        Solution obj=new Solution();
+        t.root= obj.insertNode(t.root, k);
+        t.print(t.root);
+    }
+}
