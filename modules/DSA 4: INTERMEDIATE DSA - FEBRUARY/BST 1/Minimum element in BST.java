494628f37e718a8b7fe4812dc0a46954d67d7e3c
Shubham Kumar SInha <44627084+shubhamsinha2009@users.noreply.github.com>
2023-02-12 18:57:20 +0530

    Create Minimum element in BST.java

---
+++ modules/DSA 4: INTERMEDIATE DSA - FEBRUARY/BST 1/Minimum element in BST.java
@@ -0,0 +1,70 @@
+
+// Question Link - https://course.acciojob.com/idle?question=8ff8e1b8-b718-47b4-9455-06b354499a8c
+
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int val;
+    Node left, right;
+    Node(int val){
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+class BST{
+    Node root = null;
+    BST(){
+        
+    }
+    BST(Node root){
+        root=root;
+    }
+    Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.val == val)
+            return root;
+        if(val < root.val){
+            root.left = insert(root.left, val);
+        }else{
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    void print(Node root){
+        System.out.print(root.val + " ");
+        if(root.left != null){
+            print(root.left);
+        }
+        if(root.right != null){
+            print(root.right);
+        }
+    }
+}
+
+class Solution{
+    public int minimumNode(Node root){
+
+		while(root.left!=null) root = root.left;
+		return root.val;
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        BST t = new BST();
+        for(int i = 0; i < n; i++){
+            t.root = t.insert(t.root, input.nextInt());
+        }
+        Solution obj=new Solution();
+        int result= obj.minimumNode(t.root);
+        System.out.print(result);
+        
+    }
+}
